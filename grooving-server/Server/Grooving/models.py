b2b21df127b5f7c2afc4be7e62a5461384036dff
McAllisterDTur <joslopcar@alum.us.es>
2019-03-19 00:59:43 +0100

    Modelos actualizados a falta de los actores del sistema
    Los actores del sistema hay que replantearlos quizas haciendo uso de un actor con authentication customizado:
    https://docs.djangoproject.com/en/2.1/topics/auth/customizing/

--- grooving-server/Server/Grooving/models.py
+++ grooving-server/Server/Grooving/models.py
@@ -1,7 +1,11 @@
+from collections import defaultdict
+
 from django.contrib.auth.models import User
 from django.db import models
-from django.core.validators import MinValueValidator,MaxValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.contrib.postgres.fields import ArrayField
+from enum import Enum
+
 
 # Create your models here.
 class UserAbstract(models.Model):
@@ -21,27 +25,97 @@ class Artist(models.Model):
     def __str__(self):
         return self.user.actor.username
 
+class CreditCardField():
+    holder = models.CharField(blank=False, null=False)
+    expirationDate = models.DateField(blank=False, null=False, allow_future=True)
+    number = models.CharField(blank=False, null=False, max_length=16)
+    cvv = models.CharField(blank=False, null=False, max_length=3)
+
+    def __init__(self, holder, expirationDate, number, cvv):
+        self.holder = str(holder)
+        self.expirationDate = expirationDate
+        number = number
+        cvv = cvv
 
 class Customer(models.Model):
     user = models.OneToOneField(UserAbstract, blank=False, null=False, on_delete=models.CASCADE)
-    creditCard =
+    creditcard = CreditCardField(blank=True, null=True)
+
     def __str__(self):
         return self.user.actor.username
 
+
 class Calendar(models.Model):
     year = models.IntegerField(null=False, validators=[MinValueValidator(2019), MaxValueValidator(3000)])
-    days = ArrayField(models.IntegerField(),size=)
+    days = ArrayField(models.BooleanField(default=False), size=366)
 
     def __str__(self):
         return self.year
 
+
+class ArtisticGender(models.Model):
+    name = models.CharField(blank=False, null=False, max_length=140)
+    parentGender = models.ManyToOneField('self', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Zone(models.Model):
+    name = models.CharField(blank=False, null=False, max_length=140)
+    parentZone = models.ManyToOneField('self', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Portfolio(models.Model):
     artisticName = models.CharField(blank=True, null=True, max_length=140)
-    calendar =  models.OneToManyField(Calendar, blank=True, null=True, on_delete=models.CASCADE)
-    artisticGender =  models.ManyToManyField(ArtisticGender, blank=True, null=True, on_delete=models.CASCADE)
-    portfolioModule =  models.ManyToManyField(PortfolioModule, blank=True, null=True, on_delete=models.CASCADE)
-    zone =  models.ManyToManyField(Zone, blank=True, null=True, on_delete=models.CASCADE)
-    hiring =  models.OneToManyField(Hiring, blank=True, null=True, on_delete=models.CASCADE)
+    calendar = models.OneToManyField(Calendar, blank=True, null=True, on_delete=models.CASCADE)
+    artisticGender = models.ManyToManyField(ArtisticGender, blank=True, null=True, on_delete=models.CASCADE)
+    portfolioModule = models.ManyToManyField(PortfolioModule, blank=True, null=True, on_delete=models.CASCADE)
+    zone = models.ManyToManyField(Zone, blank=True, null=True, on_delete=models.CASCADE)
+    hiring = models.OneToManyField(PaymentPackage, blank=True, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.artisticName
+
+
+class EventLocation(models.Model):
+    name = models.CharField(blank=False, null=False)
+    address = models.CharField(blank=False, null=False)
+    equipment = models.TextField(blank=False, null=False)
+    description = models.TextField(blank=True, null=True)
+    zone = models.ManyToOneField(Zone, blank=False, null=False)
+    customer = models.ManyToOneField(Customer, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class OfferStatusField(Enum):
+    PENDING = "PENDING"
+    NEGOTIATION = "NEGOTIATION"
+    CONTRACT_MADE = "CONTRACT_MADE"
+    WITHDRAWN = "WITHDRAWN"
+    REJECTED = "REJECTED"
+    CANCELED = "CANCELED"
+    PAYMENT_MADE = "PAYMENT_MADE"
+
+
+class Offer(models.Model):
+    description = models.TextField(blank=False, null=False)
+    status = models.CharField(language=models.CharField(
+        max_length=15,
+        choices=[(tag, tag.value) for tag in OfferStatusField]
+    ))
+    date = models.DateTimeField(null=False, blank=False)
+    hours = models.FloatField(blank=False, null=False)
+    price = models.FloatField(blank=False, null=False)
+    paymentCode = models.CharField(max_length=140)
+    paymentPackage = models.OneToOneField(PaymentPackage, blank=False, null=False)
+    eventLocation = models.OneToManyField(EventLocation, blank=False, null=False)
+
+    def __str__(self):
+        return self.description
+
