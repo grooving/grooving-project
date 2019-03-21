8a4bb642149102c7256c9b76fe6b3ef7fecff550
Alicia <aliviaord@alum.us.es>
2019-03-21 18:25:18 +0100

    Date selection view first version Adds a view for selecting the date when the show will take place. It also adds a Calendar component. Some changes will be added to the desktop version in the future.

---
+++ grooving-client/src/components/Calendar.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+        <div><vuejs-datepicker v-model="model.date" :disabledDates="disabledFn" :full-month-name="true" :inline="true"></vuejs-datepicker></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Calendar",
+    props: {
+    },
+    components: {
+        vuejsDatepicker
+    },
+    data () {
+        return {
+            model: {
+                date: ""
+            },
+            disabledFn: {
+                customPredictor (date) {
+                    if (date.getTime() <= new Date(Date.now() - 8640000)) {
+                        return true;
+                    }
+
+                    if (date.getDate() % 7 === 0) {
+                        return true;
+                    }
+                }
+            },
+        }
+    },
+}
+</script>
+
+<style>
+
+    .vdp-datepicker__calendar header span {
+        font-weight: bold;
+    }
+
+    .vdp-datepicker__calendar .cell.day-header {
+        color: #187FE6;
+        font-weight: bold;
+    }
+
+    .vdp-datepicker__calendar .cell:not(.blank):not(.disabled).day:hover, .vdp-datepicker__calendar .cell:not(.blank):not(.disabled).month:hover, .vdp-datepicker__calendar .cell:not(.blank):not(.disabled).year:hover {
+        border: none !important;
+    }
+
+    .vdp-datepicker__calendar .cell.selected {
+        background-image: linear-gradient(to right, #00fb82, #187fe6);
+        
+        border: none;
+        border-radius: 30px;
+
+        color: #ffffff;
+    }
+
+    .vdp-datepicker__calendar .cell.selected:hover {
+        background-image: linear-gradient(to right, #00fb82, #187fe6);
+    }
+
+</style>
+
+<style scoped>
+    * {
+        color: #000000;
+    }
+</style>
+
