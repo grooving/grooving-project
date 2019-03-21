a17a922ccf4ff0dedc4b1efd9f232ad5c8d85247
chainwarden16 <whiteraven1000@gmail.com>
2019-03-21 09:32:26 +0100

    Adición de las entidades en el panel de administración
    Se ha añadido todas las entidades en el panel de administración de Django para editarlas con facilidad y hacer pruebas.

--- grooving-server/Server/Grooving/admin.py
+++ grooving-server/Server/Grooving/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Artist, Portfolio, PortfolioModule, ArtisticGender, Customer, Offer, EventLocation, CreditCardField, SystemConfiguration, Custom, Fare, Performance, MoneyField, PaymentPackage, Zone, Calendar
 
-# Register your models here.
+admin.site.register(Artist)
+admin.site.register(Portfolio)
+admin.site.register(PortfolioModule)
+admin.site.register(ArtisticGender)
+admin.site.register(Customer)
+admin.site.register(Offer)
+admin.site.register(EventLocation)
+admin.site.register(CreditCardField)
+admin.site.register(SystemConfiguration)
+admin.site.register(Custom)
+admin.site.register(Fare)
+admin.site.register(Performance)
+admin.site.register(MoneyField)
+admin.site.register(PaymentPackage)
+admin.site.register(Zone)
+admin.site.register(Calendar)
