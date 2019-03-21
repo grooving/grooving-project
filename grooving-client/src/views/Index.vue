1aa3bfa179c8aefa39753beb10b1aec9b78f3bff
Tu Nombre <you@example.com>
2019-03-21 16:15:17 +0100

    Vista de Index Se ha añadido la vista para la página de inicio de Grooving

---
+++ grooving-client/src/views/Index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="carouselExampleControls" style="border: 3px !important;" class="carousel slide" data-ride="carousel">
+      <div style="box-shadow: 0px 0px 10px 0 rgba(0, 0, 0, .2)"  class="carousel-inner">
+        <div  class="carousel-item active">
+          <img class="d-block w-100" src="@/assets/img/banner.png" alt="First slide">
+        </div>
+      </div>
+    </div>
+</template>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+
+<script>
+  
+// @ is an alias to /src
+
+export default {
+  name: 'Index'
+}
+</script>
+
+<style>
+  img{
+    border-radius: 25px;
+  }
+
+  *{
+    background-color: red !important;
+  }
+</style>
