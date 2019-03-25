ec30786870c11959dcfc49b68ab8c3b127b6b883
josembell <you@example.com>
2019-03-25 23:34:12 +0100

    Offers fixed: josembell

--- grooving-client/src/components/Offer.vue
+++ grooving-client/src/components/Offer.vue
@@ -2,29 +2,25 @@
     <div class="everything">
         <div class="card tarjeta">
             <div class="container-fluid">
-                <div class="row" style="padding-top: 10px">
-                    <div class="col-8 texto-izq"> 
-                        <span class="card-title"><strong>Offer #{{ offerID }} - {{price}}€</strong></span>
-                    </div>
-                    <div class="col-4 texto-dcha">
-                        <i class="material-icons align-middle" style="font-family: 'Material Icons'">date_range</i>
-                        <span>Date</span>
-                    </div>
-                </div>
-                <div class="row subtitle">
-                    <div class="col-6 texto-izq"> 
-                        <p> In {{place}}</p>
-                    </div>
-                    <div class="col-6 texto-dcha">
+                <div class="container">
+                    <div class="right-div right-text" style="width: 100px; text-align: left; vertical-align: top;s">
+                        <span class="card-title"><h2>Offer #{{ offerID }} - {{price}}€</h2></span><br>
+                        
                         <p>{{date}}</p>
+                        <p>  In {{place}}</p>
+                    </div>
+                    <div class="right-div right-text">
+                        <img class="foto" src="https://img.europapress.es/fotoweb/fotonoticia_20181107115306_1920.jpg">
+                        <h3 class="fotoText">PEPITO MENGANITO</h3>
                     </div>
                 </div>
-                <div class="row botonera">
-                <div class="rejectButtonDiv"><router-link v-bind:to="rejectURI" class="btn btn-primary rejectButton"><span class="continueText">REJECT</span></router-link></div>
-                <div class="confirmButtonDiv"><router-link v-bind:to="confirmURI" class="btn btn-primary confirmButton"><span class="continueText">ACCEPT</span></router-link></div>
+                <div class="row container" >
+                <div class="right-div right-text2"><router-link v-bind:to="rejectURI" class="btn btn-primary rejectButton"><span class="continueText">REJECT</span></router-link></div>
+                <div class="right-div right-text2"><router-link v-bind:to="confirmURI" class="btn btn-primary confirmButton"><span class="continueText">ACCEPT</span></router-link></div>
             </div>
             </div>
         </div>
+    </div>
     </div>
 </template>
 
@@ -64,13 +60,48 @@
 </script>
 
 <style scoped>
+
+    .row .container{
+        margin: 0px !important;
+    }
+
+    .foto{
+        width: 100%;
+        border-radius: 10px;
+        box-shadow: 0px 2px 8px 0px rgba(0, 0, 0, .3);
+    }
+
+    .fotoText{
+        margin-top: 10%;
+    }
+
+    .right-div{
+        display: inline-block;
+        text-align: center;
+        width: 50% !important;
+        padding: 20px !important;
+    }
+    
+    .right-div2{
+        padding: 0px !important;
+    }
+
+    .right-div2{
+        padding: 10px !important;
+    }
+
+    .right-div .right-text{
+        width: 200px;
+    }
+
     * {
         font-family: "Archivo"
     }
 
     .botonera{
-        padding-left: 10px; 
-        padding-bottom:10px
+        
+        display:flex;
+        align-content: center;
     }
 
     .subtitle{
@@ -86,6 +117,11 @@
 
     .card-title{
         font-size: 1.25rem;
+        
+    }
+
+    .card-title h2{
+        font-weight: bold !important;
     }
 
     .tarjeta {
@@ -100,17 +136,20 @@
     .rejectButtonDiv, .confirmButtonDiv {
         padding: 5px;
     }
+    
+    
 
     .bothButtons {
         display: flex;
         justify-content: center;
         padding-bottom: 20px;
+        width: 100% !important;
     }
 
     .confirmButton, .rejectButton {
         font-size: 21px;
         font-weight:bold;
-        
+        width: 100%;
         border: none;
         border-radius: 30px;
     }
