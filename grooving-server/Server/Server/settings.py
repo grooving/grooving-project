51374252ad9d449755096cc43cf8b301526483fa
chainwarden16 <whiteraven1000@gmail.com>
2019-03-25 15:18:12 +0100

    Tests de listar artistas
    Se ha subido los tests para listar los artistas de la aplicación.

--- grooving-server/Server/Server/settings.py
+++ grooving-server/Server/Server/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Grooving.apps.GroovingConfig',
+    'artist.apps.ArtistConfig',
     'django_filters',
     'rest_framework',
     'rest_framework_swagger',
@@ -99,10 +100,10 @@ DATABASES = {
         'NAME': 'grooving',
         'HOST': '127.0.0.1',
         'PORT': '5432',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        #'USER': 'grooving',
-        #'PASSWORD': 'grooving',
+        #'USER': 'root',
+        #'PASSWORD': 'root',
+        'USER': 'grooving',
+        'PASSWORD': 'grooving',
         'TEST': {
             'NAME': 'test_grooving',
         }
--- grooving-server/Server/artist/tests.py
+++ grooving-server/Server/artist/tests.py
@@ -1,90 +1,220 @@
 from django.test import TestCase
 
-from Grooving.models import Portfolio, PortfolioModule, ArtisticGender, MoneyField, Customer, CreditCardField, Artist, Portfolio, User, Calendar,PaymentPackage,EventLocation,Zone
+from Grooving.models import ArtisticGender, Artist, Portfolio, User, PaymentPackage
 from datetime import datetime
 from rest_framework.test import APITestCase
 # Create your tests here.
 
 
-class PortfolioTestCase(TestCase):
+class ListArtistTestCase(TestCase):
 
-    def test_show_portfolio(self):
+    def test_list_artists(self):
 
-        credit_card1 = CreditCardField('Pedro Rodriguez', datetime.now(), '1234567890123456', '203')
-        user1 = User()
-        user1.username = "pedro"
-        user1.id = "42"
-        user1.email = "pedro@pedro.com"
-        user1.password = "pedro"
-        user1.save()
+        user = User()
+        user.email = "juan@juan.com"
+        user.username = "artist"
+        user.password= "artist"
+        user.id = "43"
+        user.save()
 
-        days = [True] * 366
-        print(days)
-        calendar1 = Calendar(year=2019, days=days)
-        calendar1.id = "44"
-        calendar1.save()
+        artista = Artist()
+        artista.photo = "https://conectandomeconlau.com.co/wp-content/uploads/2018/03/%C2%BFTienes-las-caracteri%CC%81sticas-para-ser-un-Artista.png"
+        artista.id ="56"
+        artista.iban = "AD1400080001001234567890"
+        artista.phone = "999999999"
+        artista.paypalAccount = "user=artist,password=artist"
+        artista.user_id = "43"
+        artista.save()
 
-        user2 = User()
-        user2.email = "juan@juan.com"
-        user2.username = "artist"
-        user2.password= "artist"
-        user2.id = "43"
-        user2.save()
+        response = self.client.get('/artists/'.format(1), format='json')
+        self.assertEqual(response.status_code, 200)
+        item_dict = response.json()
+        self.assertTrue(len(item_dict['results']) == 1)
+        print(response)
 
-        artist1 = Artist()
-        artist1.photo = "https://conectandomeconlau.com.co/wp-content/uploads/2018/03/%C2%BFTienes-las-caracteri%CC%81sticas-para-ser-un-Artista.png"
-        artist1.id ="56"
-        artist1.iban = "AD1400080001001234567890"
-        artist1.phone = "999999999"
-        artist1.paypalAccount = "user=artistpay,password=artistpay"
-        artist1.user_id = "43"
+    def test_list_artists_filter_artistic_name(self):
 
-        artisticGender1 = ArtisticGender()
-        artisticGender1.name = "Rock"
-        artisticGender1.save()
-
-        artisticGender2 = ArtisticGender()
-        artisticGender2.name = "Blues"
-        artisticGender2.parentGender = id
-        artisticGender2.save()
-
-        portfolio1 = Portfolio()
-        portfolio1.id = "24"
-        portfolio1.artisticName = "Juanartist"
-        portfolio1.save()
-
-        portfolioModule1 = PortfolioModule()
-        portfolioModule1.type = "PHOTO"
-        portfolioModule1.link = "https://conectandomeconlau.com.co/wp-content/uploads/2018/03/%C2%BFTienes-las-caracteri%CC%81sticas-para-ser-un-Artista.png"
-        portfolioModule1.description = "Modulo Photo"
-        portfolioModule1.portfolio = "24"
-        portfolioModule1.save()
-
-        portfolioModule2 = PortfolioModule()
-        portfolioModule2.type = 'VIDEO'
-        portfolioModule2.link = "https://conectandomeconlau.com.co/wp-content/uploads/2018/03/%C2%BFTienes-las-caracteri%CC%81sticas-para-ser-un-Artista.png"
-        portfolioModule2.description = "Modulo Video"
-        portfolioModule2.portfolio = "24"
-        portfolioModule2.save()
-
+        user = User()
+        user.email = "juan@juan.com"
+        user.username = "artist"
+        user.password= "artist"
+        user.id = "43"
+        user.save()
 
         paymentPackage = PaymentPackage()
         paymentPackage.id = "87"
         paymentPackage.description = "paymentPackage description"
         paymentPackage.appliedVAT = "0.07"
-        paymentPackage.portfolio = "24"
         paymentPackage.save()
 
-        zone1 = Zone()
-        zone1.id = "50"
-        zone1.name = "Sevilla Sur"
-        zone1.save()
+        portfolio1 = Portfolio()
+        portfolio1.id = "2"
+        portfolio1.artisticName = "Jose"
+        portfolio1.paymentPackage = paymentPackage
+        portfolio1.save()
 
-        artist1.portfolio = "24"
-        artist1.save()
+        artista = Artist()
+        artista.photo = "https://conectandomeconlau.com.co/wp-content/uploads/2018/03/%C2%BFTienes-las-caracteri%CC%81sticas-para-ser-un-Artista.png"
+        artista.id ="56"
+        artista.iban = "AD1400080001001234567890"
+        artista.phone = "999999999"
+        artista.paypalAccount = "user=artist,password=artist"
+        artista.user_id = "43"
+        artista.portfolio = portfolio1
+        artista.save()
 
-        self.client.login(username='artist', password='artist')
-        response = self.client.get('/portfolio/24', format='json')
-        '''self.assertEqual(response.status_code, 200)'''
+        user2 = User()
+        user2.email = "juanjo@juanjo.com"
+        user2.username = "artist2"
+        user2.password = "artist2"
+        user2.id = "44"
+        user2.save()
+
+        paymentPackage2 = PaymentPackage()
+        paymentPackage2.id = "88"
+        paymentPackage2.description = "paymentPackage description 2222222222"
+        paymentPackage2.appliedVAT = "0.07"
+        paymentPackage2.save()
+
+        portfolio2 = Portfolio()
+        portfolio2.id = "3"
+        portfolio2.artisticName = "María se fue a la cama a las diez"
+        portfolio2.paymentPackage = paymentPackage2
+        portfolio2.save()
+
+        artista2 = Artist()
+        artista2.photo = "https://conectandomeconlau.com.co/wp-content/uploads/2018/03/%C2%BFTienes-las-caracteri%CC%81sticas-para-ser-un-Artista.png"
+        artista2.id = "57"
+        artista2.iban = "AD1400080001001234567890"
+        artista2.phone = "999999999"
+        artista2.paypalAccount = "user=artist,password=artist"
+        artista2.user_id = "44"
+        artista2.portfolio = portfolio2
+        artista2.save()
+
+        response = self.client.get('/artists/?artisticName=Jose', format='json')
+        item_dict = response.json()
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(len(item_dict['results']) == 1)
         print(response)
 
+    def test_list_artists_filter_artistic_gender(self):
+
+        user = User()
+        user.email = "juan@juan.com"
+        user.username = "artist"
+        user.password= "artist"
+        user.id = "43"
+        user.save()
+
+        paymentPackage = PaymentPackage()
+        paymentPackage.id = "87"
+        paymentPackage.description = "paymentPackage description"
+        paymentPackage.appliedVAT = "0.07"
+        paymentPackage.save()
+
+        artisticGender1 = ArtisticGender()
+        artisticGender1.name = "Rock"
+        artisticGender1.parentGender = None
+        artisticGender1.id = "3"
+        artisticGender1.save()
+
+        portfolio1 = Portfolio()
+        portfolio1.id = "2"
+        portfolio1.artisticName = "Jose"
+        portfolio1.paymentPackage = paymentPackage
+        portfolio1.save()
+
+        portfolio1.artisticGender.add(artisticGender1)
+        portfolio1.save()
+
+        artista = Artist()
+        artista.photo = "https://conectandomeconlau.com.co/wp-content/uploads/2018/03/%C2%BFTienes-las-caracteri%CC%81sticas-para-ser-un-Artista.png"
+        artista.id ="56"
+        artista.iban = "AD1400080001001234567890"
+        artista.phone = "999999999"
+        artista.paypalAccount = "user=artist,password=artist"
+        artista.user_id = "43"
+        artista.portfolio = portfolio1
+        artista.save()
+
+        user2 = User()
+        user2.email = "juanjo@juanjo.com"
+        user2.username = "artist2"
+        user2.password = "artist2"
+        user2.id = "44"
+        user2.save()
+
+        paymentPackage2 = PaymentPackage()
+        paymentPackage2.id = "88"
+        paymentPackage2.description = "paymentPackage description 2222222222"
+        paymentPackage2.appliedVAT = "0.07"
+        paymentPackage2.save()
+
+        portfolio2 = Portfolio()
+        portfolio2.id = "3"
+        portfolio2.artisticName = "María se fue a la cama a las diez"
+        portfolio2.paymentPackage = paymentPackage2
+        portfolio2.save()
+
+        artista2 = Artist()
+        artista2.photo = "https://conectandomeconlau.com.co/wp-content/uploads/2018/03/%C2%BFTienes-las-caracteri%CC%81sticas-para-ser-un-Artista.png"
+        artista2.id = "57"
+        artista2.iban = "AD1400080001001234567890"
+        artista2.phone = "999999999"
+        artista2.paypalAccount = "user=artist,password=artist"
+        artista2.user_id = "44"
+        artista2.portfolio = portfolio2
+        artista2.save()
+
+        response = self.client.get('/artists/?artisticGender=Rock', format='json')
+        item_dict = response.json()
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(len(item_dict['results']) == 1)
+        print(response)
+
+    def test_list_artists_filter_artistic_gender_no_matches(self):
+
+        artisticGender1 = ArtisticGender()
+        artisticGender1.name = "Rock"
+        artisticGender1.parentGender = None
+        artisticGender1.id = "3"
+        artisticGender1.save()
+
+        user2 = User()
+        user2.email = "juanjo@juanjo.com"
+        user2.username = "artist2"
+        user2.password = "artist2"
+        user2.id = "44"
+        user2.save()
+
+        paymentPackage2 = PaymentPackage()
+        paymentPackage2.id = "88"
+        paymentPackage2.description = "paymentPackage description 2222222222"
+        paymentPackage2.appliedVAT = "0.07"
+        paymentPackage2.save()
+
+        portfolio2 = Portfolio()
+        portfolio2.id = "3"
+        portfolio2.artisticName = "María se fue a la cama a las diez"
+        portfolio2.paymentPackage = paymentPackage2
+        portfolio2.save()
+
+        portfolio2.artisticGender.add(artisticGender1)
+        portfolio2.save()
+
+        artista2 = Artist()
+        artista2.photo = "https://conectandomeconlau.com.co/wp-content/uploads/2018/03/%C2%BFTienes-las-caracteri%CC%81sticas-para-ser-un-Artista.png"
+        artista2.id = "57"
+        artista2.iban = "AD1400080001001234567890"
+        artista2.phone = "999999999"
+        artista2.paypalAccount = "user=artist,password=artist"
+        artista2.user_id = "44"
+        artista2.portfolio = portfolio2
+        artista2.save()
+
+        response = self.client.get('/artists/?artisticGender=Country', format='json')
+        item_dict = response.json()
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(len(item_dict['results']) == 0)
+        print(response)
