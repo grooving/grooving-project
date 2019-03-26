7579d89892b7ab3794a78fabb53925e84dc98ad6
Alicia <aliviaord@alum.us.es>
2019-03-26 13:54:41 +0100

    <feat> Rejection Reason Dropdown for pending and accepted offers, where you can add a rejection reason.

--- grooving-client/src/components/Offer.vue
+++ grooving-client/src/components/Offer.vue
@@ -14,10 +14,24 @@
                         <h3 class="fotoText">PEPITO MENGANITO</h3>
                     </div>
                 </div>
-                <div class="row container" >
-                <div class="right-div right-text2"><router-link v-bind:to="rejectURI" class="btn btn-primary rejectButton"><span class="continueText">REJECT</span></router-link></div>
-                <div class="right-div right-text2"><router-link v-bind:to="confirmURI" class="btn btn-primary confirmButton"><span class="continueText">ACCEPT</span></router-link></div>
-            </div>
+                <div class="collapse" v-bind:id="noHashtag()">
+                    <div class="form-group" style="text-align: left;">
+                        <label for="exampleFormControlTextarea1" style="font-weight:bold;">Please, confirm your rejection:</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="You can explain the reason why you are rejecting this offer. It will be shown to the person that contacted you."></textarea>
+                    </div>
+                    <div class="row container">
+                        <div class="right-div right-text2"><a v-bind:href="hashtag()" v-on:click="enableOfferButtons()" class="btn btn-primary cancelButton" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="collapseExample"><span class="continueText">CANCEL</span></a></div>
+                        <div class="right-div right-text2"><router-link v-bind:to="confirmURI" class="btn btn-primary confirmButton"><span class="continueText">CONFIRM</span></router-link></div>
+                    </div>
+                </div>
+                <div v-if="offerStatus === 'pending' || offerStatus === 'accepted'" class="row container" v-bind:id="buttonsId()">
+                    <div class="right-div right-text2"><a v-bind:href="hashtag()" v-on:click="disableOfferButtons()" class="btn btn-primary rejectButton" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="collapseExample"><span class="continueText">REJECT</span></a></div>
+                    <div class="right-div right-text2"><router-link v-bind:to="confirmURI" class="btn btn-primary confirmButton"><span class="continueText">ACCEPT</span></router-link></div>
+                </div>
+                <div v-else class="row container" v-bind:id="buttonsId()">
+                    <div class="right-div right-text2"><a v-bind:href="hashtag()" class="btn btn-primary rejectButton"><span class="continueText">REJECT</span></a></div>
+                    <div class="right-div right-text2"><router-link v-bind:to="confirmURI" class="btn btn-primary confirmButton"><span class="continueText">ACCEPT</span></router-link></div>
+                </div>
             </div>
         </div>
     </div>
@@ -53,8 +67,32 @@
             confirmURI: {
                 type: String,
                 default: '#'
+            },
+            offerStatus: {
+                type: String,
+                default: 'a',
             }
         },
+
+        methods: {
+            hashtag() {
+                return "#offer" + this.offerID;
+            },
+            noHashtag() {
+                return "offer" + this.offerID;
+            },
+            buttonsId() {
+                return "offerButtons" + this.offerID;
+            },
+            disableOfferButtons() {
+                document.getElementById(this.buttonsId()).style.display='none';
+                return false;
+            },
+            enableOfferButtons() {
+                document.getElementById(this.buttonsId()).style.display='inline-block';
+                return false;
+            }
+        }
     }   
 
 </script>
@@ -146,7 +184,7 @@
         width: 100% !important;
     }
 
-    .confirmButton, .rejectButton {
+    .confirmButton, .rejectButton, .cancelButton {
         font-size: 21px;
         font-weight:bold;
         width: 100%;
@@ -162,12 +200,20 @@
         background-image: linear-gradient(to right, #FB8600, #FF0000);
     }
 
+    .cancelButton {
+        background-image: linear-gradient(to right, #a2a2a2, #474747);
+    }
+
     .confirmButton:hover {
         background-image: linear-gradient(to right, #14Ca9f, #1648d0) !important;
     }
 
     .rejectButton:hover {
         background-image: linear-gradient(to right, #ED7F00, #A20101) !important;
+    }
+
+    .cancelButton:hover {
+        background-image: linear-gradient(to right, #515151, #232323) !important;
     }
 
     @media (min-width:768px)  {
