8dc5764bbda4001cd7c5ac62ba59dce88b68947f
Alicia <aliviaord@alum.us.es>
2019-03-26 18:26:08 +0100

    <feat> Profile info There's a profile info card component and a personal info view.

---
+++ grooving-client/src/views/PersonalInfo.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="prueba">
+    <div class="everything">
+        <div class="paymentSelect">
+          <div class="paymentOptions"><ProfileInfo/></div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+import ProfileInfo from '@/components/ProfileInfo.vue'
+
+export default {
+    name: 'personalInfo',
+    components: {
+        ProfileInfo
+    },
+}
+</script>
+
+<style scoped>
+    * {
+        font-family: "Archivo"
+    }
+
+    @media (min-width:768px)  {
+      
+        .everything {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            margin-top: 5%;
+            text-align: center;
+            padding: 15px;
+            margin-left: 10%;
+            margin-right: 10%;
+            margin-top:0%;
+        }
+    }
+
+</style>
