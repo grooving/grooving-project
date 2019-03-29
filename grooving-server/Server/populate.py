8ed668235d1ad9c65274f540b14008350b262212
Juan Carlos Utrilla <utri1990@gmail.com>
2019-03-29 16:11:15 +0100

    Populate calendar fixed

--- grooving-server/Server/populate.py
+++ grooving-server/Server/populate.py
@@ -205,25 +205,25 @@ def save_data():
     calendar1 = Calendar.objects.create(days=availableDays, portfolio=portfolio1)
     calendar1.save()
 
-    calendar2 = Calendar.objects.create(days=availableDays, portfolio=portfolio1)
+    calendar2 = Calendar.objects.create(days=availableDays, portfolio=portfolio2)
     calendar2.save()
 
-    calendar3 = Calendar.objects.create(days=availableDays, portfolio=portfolio2)
+    calendar3 = Calendar.objects.create(days=availableDays, portfolio=portfolio3)
     calendar3.save()
 
-    calendar4 = Calendar.objects.create(days=availableDays, portfolio=portfolio2)
+    calendar4 = Calendar.objects.create(days=availableDays, portfolio=portfolio4)
     calendar4.save()
 
-    calendar5 = Calendar.objects.create(days=availableDays, portfolio=portfolio3)
+    calendar5 = Calendar.objects.create(days=availableDays, portfolio=portfolio5)
     calendar5.save()
 
-    calendar6 = Calendar.objects.create(days=availableDays, portfolio=portfolio3)
+    calendar6 = Calendar.objects.create(days=availableDays, portfolio=portfolio6)
     calendar6.save()
 
-    calendar7 = Calendar.objects.create(days=availableDays, portfolio=portfolio4)
+    calendar7 = Calendar.objects.create(days=availableDays, portfolio=portfolio7)
     calendar7.save()
 
-    calendar8 = Calendar.objects.create(days=availableDays, portfolio=portfolio4)
+    calendar8 = Calendar.objects.create(days=availableDays, portfolio=portfolio8)
     calendar8.save()
 
     # Users...
