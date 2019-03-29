2a8fdf5b4a7e0c562bcfb3e5945427da2a518400
Juan Carlos Utrilla <utri1990@gmail.com>
2019-03-29 05:21:11 +0100

    CorsMiddleware.py updated

--- grooving-server/Server/utils/CorsMiddleware.py
+++ grooving-server/Server/utils/CorsMiddleware.py
@@ -6,4 +6,9 @@ class CorsMiddleware:
     def __call__(self, request):
         response = self.get_response(request)
         response['Access-Control-Allow-Origin'] = "http://localhost:8080"
+        response['Access-Control-Allow-Methods'] = "GET,POST,PUT,OPTIONS"
+        response['Access-Control-Expose-Headers'] = 'x-auth'
+        response['Access-Control-Allow-Headers'] = "Authorization,access-control-allow-origin, access-control-allow-headers, access-control-allow-methods, content-type, access-control-allow-credentials"
+        response['Access-Control-Allow-Credentials'] = "true"
+        response['JoseAntonio'] = 'true'
         return response
