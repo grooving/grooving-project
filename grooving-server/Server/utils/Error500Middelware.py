d1999ef5864048c1c6a8c07b5e3c3390e26de863
Iriabow <guilderwars@gmail.com>
2019-03-29 02:07:28 +0100

    Middleware to controll http 500

---
+++ grooving-server/Server/utils/Error500Middelware.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponseForbidden
+
+
+class Erro500Middleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        if response.status_code == 500:
+            return HttpResponseForbidden()
+        return response
