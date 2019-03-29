7af8829b6adf20159717dc0c07c2c308dbe10384
Iriabow <guilderwars@gmail.com>
2019-03-29 05:44:27 +0100

    More data on list offers

--- grooving-server/Server/offers/serializers.py
+++ grooving-server/Server/offers/serializers.py
@@ -1,7 +1,33 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-from Grooving.models import Offer
-from eventLocation.serializers import EventLocationSerializer
+from Grooving.models import Offer, PaymentPackage, Customer,Zone, EventLocation, UserAbstract
+from eventLocation.serializers import ZoneSerializer
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        depth = 1
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'email')
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    class Meta:
+        depth = 1
+        model = Customer
+        fields = ('user', 'holder','photo')
+
+
+class EventLocationSerializer(serializers.ModelSerializer):
+    zone = ZoneSerializer(read_only=True)
+    zone_id = serializers.PrimaryKeyRelatedField(write_only=True, queryset=Zone.objects.all(),
+                                                           source='zone')
+    customer = CustomerSerializer(read_only=True)
+
+    class Meta:
+        model = EventLocation
+        fields = ('id', 'name', 'address', 'equipment', 'description', 'zone', 'zone_id', 'customer')
 
 
 class ListOfferSerializer(serializers.HyperlinkedModelSerializer):
@@ -10,4 +36,4 @@ class ListOfferSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Offer
-        fields = ('id', 'description', 'status', 'date', 'hours', 'eventLocation')
+        fields = ('id', 'description', 'status', 'price', 'date', 'hours', 'eventLocation')
